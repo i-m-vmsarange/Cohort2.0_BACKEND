291b96cf8f8b6ba9d8d17aa80eb0363d91db1eb7
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-02-05 14:46:21 +0530

    Added the update feature

--- DAY-10/Frontend/src/App.jsx
+++ DAY-10/Frontend/src/App.jsx
@@ -11,6 +11,14 @@ const App = () => {
     },
   ]);
 
+  const [editId, setEditId] = useState(null);
+
+  const [formData, setFormData] = useState({
+    username: "",
+    description: "",
+    occupation: "",
+  });
+
   function fetchUsers() {
     axios.get("http://localhost:3000/api/users").then((res) => {
       setUsers(res.data.users);
@@ -24,23 +32,70 @@ const App = () => {
 
   function handleSubmit(e) {
     e.preventDefault();
-    const { username, description, occupation } = e.target.elements;
-    // console.log(username.value, description.value, occupation.value);
-    axios
-      .post("http://localhost:3000/api/users", {
-        username: username.value,
-        description: description.value,
-        occupation: occupation.value,
-      })
-      .then((res) => {
+
+    if (editId) {
+      updateOccupation(editId).then((res) => {
+        console.log(res);
+        setFormData({
+          username: "",
+          description: "",
+          occupation: "",
+        });
+        setEditId(null);
         fetchUsers();
       });
+    } else {
+      const { username, description, occupation } = e.target.elements;
+      // console.log(username.value, description.value, occupation.value);
+      axios
+        .post("http://localhost:3000/api/users", {
+          username: username.value,
+          description: description.value,
+          occupation: occupation.value,
+        })
+        .then((res) => {
+          console.log(res);
+          setFormData({
+            username: "",
+            description: "",
+            occupation: "",
+          });
+          fetchUsers();
+        });
+    }
   }
+
   function deleteHandler(noteId) {
     axios.delete("http://localhost:3000/api/users/" + noteId).then((res) => {
       console.log(res);
       fetchUsers();
     });
+  }
+
+  function handleEdit(user) {
+    setFormData({
+      username: user.username,
+      description: user.description,
+      occupation: user.occupation,
+    });
+    setEditId(user._id);
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
+
+  async function updateOccupation(editId) {
+    await axios
+      .patch(`http://localhost:3000/api/users/${editId}`, {
+        occupation: formData.occupation,
+      })
+      .then((res) => {
+        console.log(res);
+      });
   }
 
   return (
@@ -52,18 +107,24 @@ const App = () => {
         >
           <input
             name="username"
+            value={formData.username}
+            onChange={handleChange}
             type="text"
             placeholder="Enter username"
             className="border-amber-500 border m-2 px-2 py-1 text-md rounded"
           />
           <input
             name="description"
+            value={formData.description}
+            onChange={handleChange}
             type="text"
             placeholder="Enter description"
             className="border-amber-500 border m-2 px-2 py-1 text-md rounded"
           />
           <input
             name="occupation"
+            value={formData.occupation}
+            onChange={handleChange}
             type="text"
             placeholder="Enter occupation"
             className="border-amber-500 border m-2 px-2 py-1 text-md rounded"
@@ -76,33 +137,34 @@ const App = () => {
       <div className="h-1/2 flex flex-wrap item-center justify-center">
         {users.map((user, index) => {
           return (
-            <>
-              <div
-                key={index}
-                className="text-white bg-neutral-700  w-70 m-4 p-4 rounded-md text-center"
+            <div
+              key={index}
+              className="text-white bg-neutral-700  w-70 m-4 p-4 rounded-md text-center"
+            >
+              <h2 className="text-3xl  text-wrap font-bold tracking-wide text-center mb-4.5">
+                {user.username}
+              </h2>
+              <p className="text-lg  tracking-wider mb-3">{user.description}</p>
+              <h4 className="text-2xl font-semibold tracking-wide mb-4">
+                {user.occupation}
+              </h4>
+              <button
+                onClick={() => {
+                  deleteHandler(user._id);
+                }}
+                className="px-4 py-1 bg-pink-700 text-lg ml-2 text-white rounded-md cursor-pointer active:scale-95"
               >
-                <h2 className="text-3xl  text-wrap font-bold tracking-wide text-center mb-4.5">
-                  {user.username}
-                </h2>
-                <p className="text-lg  tracking-wider mb-3">
-                  {user.description}
-                </p>
-                <h4 className="text-2xl font-semibold tracking-wide mb-4">
-                  {user.occupation}
-                </h4>
-                <button
-                  onClick={() => {
-                    deleteHandler(user._id);
-                  }}
-                  className="px-4 py-1 bg-pink-700 text-lg ml-2 text-white rounded-md cursor-pointer active:scale-95"
-                >
-                  Delete
-                </button>
-                <button className="px-4 py-1 bg-violet-800 text-lg ml-2 text-white rounded-md cursor-pointer active:scale-95">
-                  Update
-                </button>
-              </div>
-            </>
+                Delete
+              </button>
+              <button
+                onClick={() => {
+                  handleEdit(user);
+                }}
+                className="px-4 py-1 bg-violet-800 text-lg ml-2 text-white rounded-md cursor-pointer active:scale-95"
+              >
+                Update
+              </button>
+            </div>
           );
         })}
       </div>
