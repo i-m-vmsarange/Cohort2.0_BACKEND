39af13f4c7f727d376b0d395945c5d2f1d1f0e4e
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-02-23 19:34:30 +0530

    uploaded the picture with imagekit

--- DAY-13/Backend/src/controllers/post.controller.js
+++ DAY-13/Backend/src/controllers/post.controller.js
@@ -1,7 +1,21 @@
 const postModel = require("../models/post.model");
+const ImageKit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
+
+const imagekit = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
 
 async function postController(req, res) {
-  console.log(req.file);
+  // req.file.buffer comes from multer
+  const buffer = req.file.buffer;
+
+  const file = await imagekit.files.upload({
+    file: await toFile(Buffer.from(buffer), req.file.originalname),
+    fileName: req.file.originalname,
+  });
+
+  res.send({ message: "File uploaded successfully", file });
 }
 
 module.exports = {
