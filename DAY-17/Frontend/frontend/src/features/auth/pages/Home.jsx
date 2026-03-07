fe328376d4145daa42e266d1d3ddfde180b0fe46
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-03-07 11:44:46 +0530

    added slash route

---
+++ DAY-17/Frontend/frontend/src/features/auth/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <h1 style={{ textAlign: "center", marginTop: "3rem" }}>
+      Welcome to instaclone app!!!!
+    </h1>
+  );
+};
+
+export default Home;
--- DAY-17/Frontend/frontend/src/routes.jsx
+++ DAY-17/Frontend/frontend/src/routes.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
+import Home from "./features/auth/pages/Home";
 
 export const routes = createBrowserRouter([
+  { path: "/", element: <Home /> },
   {
     path: "/login",
     element: <Login />,
