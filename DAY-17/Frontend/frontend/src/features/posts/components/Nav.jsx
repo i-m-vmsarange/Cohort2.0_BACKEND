77a64ab1dd2bf288197b26ae41a30df3d430bb6d
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-03-20 12:51:39 +0530

    created create post form

--- DAY-17/Frontend/frontend/src/features/posts/components/Nav.jsx
+++ DAY-17/Frontend/frontend/src/features/posts/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from "react-router";
 
 const Nav = () => {
   return (
-    <div className="mb-4 mt-4 flex gap-2 items-center justify-center">
+    <div className=" mb-4 mt-4 flex gap-2 items-center justify-between">
       <NavLink
         className={
           "text-sm tracking-wide font-semibold bg-pink-700 px-3 py-2 w-fit rounded-md "
@@ -22,7 +22,7 @@ const Nav = () => {
       </NavLink>
       <NavLink
         className={
-          "text-sm tracking-wide font-semibold bg-emerald-700 px-3 py-2 w-fit rounded-md "
+          "text-sm tracking-wide font-semibold bg-emerald-600 px-3 py-2 w-fit rounded-md "
         }
         to={"/create-post"}
       >
--- DAY-17/Frontend/frontend/src/features/posts/pages/CreatePost.jsx
+++ DAY-17/Frontend/frontend/src/features/posts/pages/CreatePost.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <main className="w-screen h-screen flex items-center justify-center">
+      <div className="px-8 pb-8 pt-4 bg-neutral-700  rounded-md flex flex-col gap-3">
+        <h1 className="text-2xl  font-semibold">Create New Post</h1>
+        <form className="max-w-87.5">
+          <input
+            className="px-2 py-1 bg-amber-50 text-neutral-500 cursor-pointer"
+            type="file"
+            name="imgUrl"
+            id="file"
+          />
+          <input
+            className="px-2 py-1 bg-amber-50 text-black"
+            type="text"
+            placeholder="Enter caption"
+            name="caption"
+            id="caption"
+          />
+          <button style={{ backgroundColor: "#ff3328" }}>Create</button>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default CreatePost;
