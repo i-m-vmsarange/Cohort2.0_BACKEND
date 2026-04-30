27804441d1b65f08337c02830f894e9407d484c6
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-04-30 16:25:29 +0530

    verify-email

--- perplexity/src/routes/auth.routes.js
+++ perplexity/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const authRouter = express.Router();
-import { registerUser } from "../controllers/auth.controller.js";
+import { registerUser,verifyEmail } from "../controllers/auth.controller.js";
 import registerValidation from "../validation/auth.validator.js";
 
 
 authRouter.post("/register",registerValidation,registerUser);
+
+authRouter.get("/verify-email",verifyEmail);
 
 export default authRouter;
--- perplexity/src/service/mail.service.js
+++ perplexity/src/service/mail.service.js
@@ -4,13 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-         type: "OAuth2",
+        type: "OAuth2",
         user: process.env.GOOGLE_USER,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
         clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN
     }
 })
 
@@ -32,6 +34,6 @@ export default async function sendEmail({to,subject,html,text}){
 
  const details = await transporter.sendMail(mailOptions);
 
- console.log("Email sent: \n"+details)
+ return details;
 }
 
